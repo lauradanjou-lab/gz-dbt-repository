92d453ffb27102acedd7d0ec914fff371adac226
laura.danjou@gmail.com <laura.danjou@gmail.com>
2025-11-04 13:10:40 +0000

    margin by order

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,9 +1,9 @@
 select
     orders_id
     ,date_date
-    ,SUM(revenue) AS revenue
+    ,ROUND(SUM(revenue),2) AS revenue
     ,SUM(quantity) AS quantity
-    ,SUM(purchase_cost) AS purchase_cost
-    ,SUM(margin) AS margin
+    ,ROUND(SUM(purchase_cost),2) AS purchase_cost
+    ,ROUND(SUM(margin),2) AS margin
 from {{ ref('int_sales_margin') }}
 GROUP BY orders_id,date_date
