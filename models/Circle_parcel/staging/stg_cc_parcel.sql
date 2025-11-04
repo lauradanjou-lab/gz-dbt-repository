ca4e14d64268615e4b6717a379314a06461ecba6
laura.danjou@gmail.com <laura.danjou@gmail.com>
2025-11-04 15:06:47 +0000

    rename stg cc parcel

---
+++ models/Circle_parcel/staging/stg_cc_parcel.sql
@@ -0,0 +1,10 @@
+select
+    Parcel_id AS parcel_id
+    ,Parcel_tracking AS parcel_tracking
+    ,Transporter AS transporter
+    ,Priority AS priority
+    ,Date_purCHase AS date_purchase
+    ,Date_sHIpping AS date_shipping
+    ,DATE_delivery AS date_delivery 
+    ,DaTeCANcelled AS date_cancelled
+from avid-willow-475708-p2.gz_raw_data.raw_cc_parcel
