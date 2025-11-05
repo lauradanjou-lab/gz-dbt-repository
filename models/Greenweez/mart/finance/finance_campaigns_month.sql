f9e0d5c7680f58d1f4e47435dfb64963d1db1bab
laura.danjou@gmail.com <laura.danjou@gmail.com>
2025-11-05 13:44:39 +0000

    creation of finance month

--- models/Greenweez/mart/finance/finance_campaigns_month.sql
+++ models/Greenweez/mart/finance/finance_campaigns_month.sql
@@ -1,18 +1,17 @@
 select
     extract(MONTH from date_date) as datemonth
-    ,ROUND(SUM(ads_margin),2) as ads_margin 
-    ,ROUND(SUM(average_basket),2) as average_basket 
-    ,ROUND(SUM(operational_margin),2) as operational_margin
-    ,ROUND(SUM(ads_cost),2) as ads_cost
-    ,ROUND(SUM(impression),2) as ads_impression
-    ,ROUND(SUM(click),2) as ads_clicks
-    ,ROUND(SUM(total_qty_of_products_sold),2) as quantity 
-    ,ROUND(SUM(total_revenue),2) as revenue
-    ,ROUND(SUM(total_purchase_cost),2) as purchase_cost
-    ,ROUND(SUM(operational_margin),2) as operational_margin 
-    ,ROUND(SUM(total_shipping_fees),2) as shipping_fee 
-    ,ROUND(SUM(total_log_cost),2) as log_cost 
-    ,ROUND(SUM(total_shipping_fees),2) as ship_cost
+    ,IFNULL(ROUND(SUM(ads_margin),2),0) as ads_margin 
+    ,IFNULL(ROUND(SUM(average_basket),2),0) as average_basket 
+    ,IFNULL(ROUND(SUM(operational_margin),2),0) as operational_margin
+    ,IFNULL(ROUND(SUM(ads_cost),2),0) as ads_cost
+    ,IFNULL(ROUND(SUM(impression),2),0) as ads_impression
+    ,IFNULL(ROUND(SUM(click),2),0) as ads_clicks
+    ,IFNULL(ROUND(SUM(total_qty_of_products_sold),2),0) as quantity 
+    ,IFNULL(ROUND(SUM(total_revenue),2),0) as revenue
+    ,IFNULL(ROUND(SUM(total_purchase_cost),2),0) as purchase_cost
+    ,IFNULL(ROUND(SUM(total_shipping_fees),2),0) as shipping_fee 
+    ,IFNULL(ROUND(SUM(total_log_cost),2),0) as log_cost 
+    ,IFNULL(ROUND(SUM(total_shipping_fees),2),0) as ship_cost
 from {{ ref('finance_campaigns_day') }}
 group by datemonth
 order by datemonth desc
