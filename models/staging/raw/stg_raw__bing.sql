38282ddf0d80a24a334f17b27ae1a8c21657deeb
laura.danjou@gmail.com <laura.danjou@gmail.com>
2025-11-05 10:48:24 +0000

    branche 2

---
+++ models/staging/raw/stg_raw__bing.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'bing') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        paid_source,
+        campgn_name as campaign_name,
+        cast(ads_cost as FLOAT64) as ads_cost,
+        impression,
+        click
+
+    from source
+
+)
+
+select * from renamed
